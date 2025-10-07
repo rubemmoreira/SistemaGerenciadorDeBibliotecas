895b7fa32ae3edf763a832d478690918c452abbf
Rubem Moreira <rubemmoreira.rm@gmail.com>
2025-10-07 16:41:35 -0300

    Usando o Spring Boot junto com o Maven para gerenciar a estrutura e as dependências

---
+++ gerenciador-biblioteca/src/main/java/com/biblioteca/app/GerenciadorBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorBibliotecaApplication.class, args);
+	}
+
+}
