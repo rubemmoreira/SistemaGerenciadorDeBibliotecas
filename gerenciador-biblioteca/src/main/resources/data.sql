e8c8787380f9cd2e27c0e859c997808bece2954c
Rubem Moreira <rubemmoreira.rm@gmail.com>
2025-10-19 10:27:05 -0300

    Bug  do login e logout resolvido

--- gerenciador-biblioteca/src/main/resources/data.sql
+++ gerenciador-biblioteca/src/main/resources/data.sql
@@ -1,2 +1,9 @@
+CREATE TABLE IF NOT EXISTS usuario (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    nome VARCHAR(255) NOT NULL
+);
+
 INSERT INTO usuario (email, senha, nome) 
 VALUES ('admin@biblioteca.com', '12345', 'Administrador Padrão');
